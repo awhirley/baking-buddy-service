cffb4c7fb3e363de6d2e4ebd041c8c158bc1f592
Alex Whirley <alexwhirley@gmail.com>
2026-08-19 16:39:29 -0600

    Update klint line length

--- src/main/kotlin/com/bakingbuddy/services/DeltaService.kt
+++ src/main/kotlin/com/bakingbuddy/services/DeltaService.kt
@@ -8,7 +8,9 @@ import kotlin.uuid.Uuid
 class DeltaService {
   private val deltaRepository = DeltaRepositoryImpl()
 
-  suspend fun getIngredientHistory(ingredientId: Uuid): IngredientHistory = deltaRepository.getIngredientHistory(ingredientId)
+  suspend fun getIngredientHistory(ingredientId: Uuid): IngredientHistory =
+    deltaRepository.getIngredientHistory(ingredientId)
 
-  suspend fun getInstructionHistory(instructionId: Uuid): InstructionHistory = deltaRepository.getInstructionHistory(instructionId)
+  suspend fun getInstructionHistory(instructionId: Uuid): InstructionHistory =
+    deltaRepository.getInstructionHistory(instructionId)
 }
